a86599c945427bf7f828d7deea1781ef0908a36b
tanmayb104 <tanmaybansal104@gmail.com>
2020-09-01 04:04:24 +0530

    Only top roles can use bot commands

--- bot.py
+++ bot.py
@@ -45,6 +45,7 @@ async def on_ready():
 @client.event
 async def on_message(message):
 
+    #   message.guild.id
 
     role_str=[]
     role=message.author.roles
@@ -74,122 +75,124 @@ async def on_message(message):
             cur.execute("INSERT INTO DISCORDBOT (server,channel,USERNAME,MSGCNT,EMAIL,DATE) VALUES (%s,%s,%s,1,'Not Updated',%s)", (str(message.guild),str(message.channel.name), str(message.author.name), str(date.today())))
         conn.commit()
 
-    if message.content == "!users":                 # To find number of users in the channel 
-        await message.channel.send(f"# of Members: {message.guild.member_count}")
+    if(message.guild.roles[-1].id==message.author.roles[-1].id):
 
-    elif message.content == "!msgcnt":              # To find number of messages sent by each users
-        cur.execute("SELECT username, msgcnt, date from DISCORDBOT where channel = '%s' and server = '%s' " % (str(message.channel.name),str(message.guild)))
-        rows = cur.fetchall()
-        for i in rows:
-            await message.channel.send(f"{i[0]}: {i[1]}, Last msg posted on {i[2]}")
+        if message.content == "!users":                 # To find number of users in the channel 
+            await message.channel.send(f"# of Members: {message.guild.member_count}")
 
-    elif message.content == "!rstcnt":             # To reset the count of messages of each user in a channel
-        cur.execute("DELETE from DISCORDBOT where CHANNEL = '%s' and server = '%s' " % (str(message.channel.name),str(message.guild)))
-        conn.commit()
+        elif message.content == "!msgcnt":              # To find number of messages sent by each users
+            cur.execute("SELECT username, msgcnt, date from DISCORDBOT where channel = '%s' and server = '%s' " % (str(message.channel.name),str(message.guild)))
+            rows = cur.fetchall()
+            for i in rows:
+                await message.channel.send(f"{i[0]}: {i[1]}, Last msg posted on {i[2]}")
 
-    elif str(message.content)[:9] == "!del role":  # Delete messages by the roles
-        role_del = str(message.content)[9:]
-        role_del = role_del.strip()
-        cur.execute("SELECT channel, msgid, date, roles from MESSAGES where channel='%s' and server='%s' " % (str(message.channel.name),str(message.guild)))
-        rows = cur.fetchall()
-        for row in rows:
-            list_split=str(row[3]).split("!.#$%")
-            if str(role_del) in list_split:
-                messages=await message.channel.fetch_message(int(row[1]))
-                await messages.delete(delay=None)
-                cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
-                conn.commit()
+        elif message.content == "!rstcnt":             # To reset the count of messages of each user in a channel
+            cur.execute("DELETE from DISCORDBOT where CHANNEL = '%s' and server = '%s' " % (str(message.channel.name),str(message.guild)))
+            conn.commit()
 
-
-    elif str(message.content)[:4] == "!del":      # To delete messages
-        cur.execute("SELECT channel, msgid, date from MESSAGES where channel='%s' and server = '%s' " % (str(message.channel.name),str(message.guild)))
-        rows = cur.fetchall()
-        a=9999
-        b=9999
-        c=9999
-        for i in rows:
-            d=i[2]
-            a1,b1,c1=str(d).split("-")
-            a1=int(a1)
-            b1=int(b1)
-            c1=int(c1)
-            if(a>a1):
-                a=a1
-                b=b1
-                c=c1
-            elif(a==a1):
-                if(b>b1):
-                    b=b1
-                    c=c1
-                elif(b==b1):
-                    if(c>c1):
-                        c=c1
-
-        if str(message.content)[5:] == "week":   # To delete messages in the starting week
+        elif str(message.content)[:9] == "!del role":  # Delete messages by the roles
+            role_del = str(message.content)[9:]
+            role_del = role_del.strip()
+            cur.execute("SELECT channel, msgid, date, roles from MESSAGES where channel='%s' and server='%s' " % (str(message.channel.name),str(message.guild)))
+            rows = cur.fetchall()
             for row in rows:
-                d=row[2]
-                a1,b1,c1=str(d).split("-")
-                a1=int(a1)
-                b1=int(b1)
-                c1=int(c1)
-                if(0<=c and c<=7):
-                    if(a1==a and b1==b and 0<=c1 and c1<=7):
-                        messages=await message.channel.fetch_message(int(row[1]))
-                        await messages.delete(delay=None)
-                        cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
-                        conn.commit()
-
-                elif(8<=c and c<=14):
-                    if(a1==a and b1==b and 8<=c1 and c1<=14):
-                        messages=await message.channel.fetch_message(int(row[1]))
-                        await messages.delete(delay=None)
-                        cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
-                        conn.commit()
-
-                elif(15<=c and c<=21):
-                    if(a1==a and b1==b and 15<=c1 and c1<=21):
-                        messages=await message.channel.fetch_message(int(row[1]))
-                        await messages.delete(delay=None)
-                        cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
-                        conn.commit()
-
-                elif(22<=c and c<=31):
-                    if(a1==a and b1==b and 22<=c1 and c1<=31):
-                        messages=await message.channel.fetch_message(int(row[1]))
-                        await messages.delete(delay=None)
-                        cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
-                        conn.commit()
-
-        elif str(message.content)[5:] == "month":     # To delete messages in the starting month
-            for row in rows:
-                d=row[2]
-                a1,b1,c1=str(d).split("-")
-                a1=int(a1)
-                b1=int(b1)
-                c1=int(c1)
-                if(a1==a and b1==b):
+                list_split=str(row[3]).split("!.#$%")
+                if str(role_del) in list_split:
                     messages=await message.channel.fetch_message(int(row[1]))
                     await messages.delete(delay=None)
                     cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
                     conn.commit()
 
-    elif str(message.content[:6]) == "!email":       # Add the emails of the members
-        email_add=message.content[6:]
-        email_add=email_add.strip()
-        cur.execute("UPDATE DISCORDBOT set EMAIL = '%s' where channel = '%s' and username = '%s' and server = '%s' " % (str(email_add), str(message.channel.name), str(message.author.name),str(message.guild)))
-        conn.commit()
+
+        elif str(message.content)[:4] == "!del":      # To delete messages
+            cur.execute("SELECT channel, msgid, date from MESSAGES where channel='%s' and server = '%s' " % (str(message.channel.name),str(message.guild)))
+            rows = cur.fetchall()
+            a=9999
+            b=9999
+            c=9999
+            for i in rows:
+                d=i[2]
+                a1,b1,c1=str(d).split("-")
+                a1=int(a1)
+                b1=int(b1)
+                c1=int(c1)
+                if(a>a1):
+                    a=a1
+                    b=b1
+                    c=c1
+                elif(a==a1):
+                    if(b>b1):
+                        b=b1
+                        c=c1
+                    elif(b==b1):
+                        if(c>c1):
+                            c=c1
+
+            if str(message.content)[5:] == "week":   # To delete messages in the starting week
+                for row in rows:
+                    d=row[2]
+                    a1,b1,c1=str(d).split("-")
+                    a1=int(a1)
+                    b1=int(b1)
+                    c1=int(c1)
+                    if(0<=c and c<=7):
+                        if(a1==a and b1==b and 0<=c1 and c1<=7):
+                            messages=await message.channel.fetch_message(int(row[1]))
+                            await messages.delete(delay=None)
+                            cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
+                            conn.commit()
+
+                    elif(8<=c and c<=14):
+                        if(a1==a and b1==b and 8<=c1 and c1<=14):
+                            messages=await message.channel.fetch_message(int(row[1]))
+                            await messages.delete(delay=None)
+                            cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
+                            conn.commit()
+
+                    elif(15<=c and c<=21):
+                        if(a1==a and b1==b and 15<=c1 and c1<=21):
+                            messages=await message.channel.fetch_message(int(row[1]))
+                            await messages.delete(delay=None)
+                            cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
+                            conn.commit()
+
+                    elif(22<=c and c<=31):
+                        if(a1==a and b1==b and 22<=c1 and c1<=31):
+                            messages=await message.channel.fetch_message(int(row[1]))
+                            await messages.delete(delay=None)
+                            cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
+                            conn.commit()
+
+            elif str(message.content)[5:] == "month":     # To delete messages in the starting month
+                for row in rows:
+                    d=row[2]
+                    a1,b1,c1=str(d).split("-")
+                    a1=int(a1)
+                    b1=int(b1)
+                    c1=int(c1)
+                    if(a1==a and b1==b):
+                        messages=await message.channel.fetch_message(int(row[1]))
+                        await messages.delete(delay=None)
+                        cur.execute("DELETE from MESSAGES where MSGID='%s' and server = '%s' and channel = '%s' ;" % (row[1],str(message.guild),str(message.channel.name)))
+                        conn.commit()
+
+        elif str(message.content[:6]) == "!email":       # Add the emails of the members
+            email_add=message.content[6:]
+            email_add=email_add.strip()
+            cur.execute("UPDATE DISCORDBOT set EMAIL = '%s' where channel = '%s' and username = '%s' and server = '%s' " % (str(email_add), str(message.channel.name), str(message.author.name),str(message.guild)))
+            conn.commit()
 
 
-    elif message.content == "!help":                 # To show all the possible options available with the bot
-        embed = discord.Embed(title="Help on BOT", description="Some useful commands")
-        embed.add_field(name="!users",value="Returns the number of users in the channel")
-        embed.add_field(name="!email <email id>",value="Sends an email when a pull request is made")
-        embed.add_field(name="!del role <name of role>",value="Deletes the messages by members of the specified role")
-        embed.add_field(name="!del week",value="Deletes the messages in the starting week")
-        embed.add_field(name="!del month",value="Deletes the messages in the starting month")
-        embed.add_field(name="!msgcnt",value="Returns the number of messages sent by each user")
-        embed.add_field(name="!rstcnt",value="Resets the number of messages of each user to Zero")
-        await message.channel.send(content=None, embed=embed)
+        elif message.content == "!help":                 # To show all the possible options available with the bot
+            embed = discord.Embed(title="Help on BOT", description="Some useful commands")
+            embed.add_field(name="!users",value="Returns the number of users in the channel")
+            embed.add_field(name="!email <email id>",value="Sends an email when a pull request is made")
+            embed.add_field(name="!del role <name of role>",value="Deletes the messages by members of the specified role")
+            embed.add_field(name="!del week",value="Deletes the messages in the starting week")
+            embed.add_field(name="!del month",value="Deletes the messages in the starting month")
+            embed.add_field(name="!msgcnt",value="Returns the number of messages sent by each user")
+            embed.add_field(name="!rstcnt",value="Resets the number of messages of each user to Zero")
+            await message.channel.send(content=None, embed=embed)
 
 
     elif "GitHub" in str(message.author.name):      #To send emails when a Github Pull request is made
