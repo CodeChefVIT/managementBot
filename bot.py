1b1f0bae5249988645a6a13f5d7d9acdc3ba0a50
tanmayb104 <tanmaybansal104@gmail.com>
2020-11-16 15:54:31 +0530

    Checking

--- bot.py
+++ bot.py
@@ -439,7 +439,7 @@ async def on_private_channel_update(before, after):
 
 @client.event
 async def on_member_update(before, after):
-    print(after.roles)
+    print(after.roles,after.guild,after.channel)
     conn = psycopg2.connect(database = config('database'), user = config('user'), password = config('password'), host = config('host'), port = config('port'))
     print ("Opened database successfully")
     cur = conn.cursor()
